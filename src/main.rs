13b4ec4c08ee0155987c03e845fc5d9e824f5256
devHeadset <headset@devheadset.org>
2024-12-08 16:43:25 -0500

    I forgot to commit but heres the password gen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod password;
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("How long do you want the password to be?");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let length: usize = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input. Please enter a valid number.");
+            return;
+        }
+    };
+
+    let password = password::generate_password(length);
+    println!("Generated password: {}", password);
 }
---
+++ src/password.rs
@@ -0,0 +1,10 @@
+use rand::{distributions::Alphanumeric, Rng};
+
+pub fn generate_password(length: usize) -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect()
+}
+
